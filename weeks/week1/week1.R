2dff1d9c400b4229232cbe4a5c51ba6057e17468
Max Brown <max.carter-brown@aru.ac.uk>
2024-09-25 22:06:12 +0100

    Add beginnings of week1 script, to complete on Friday.

---
+++ weeks/week1/week1.R
@@ -0,0 +1,15 @@
+# week 1:
+# building a simple script to do some probability calculations
+
+# Write a function to simulate rolling two six-sided dice 
+# 1,000 times. Count how often each possible outcome occurs.
+
+# 1. We need to save the number of rolls.
+# 2. Some way of simulating dice rolls (`?sample`).
+# 3. We have two dice. Each die rolls a number. What are we interested in? The actual values, or the combined value?
+# 4. Count outcome occurrences (i.e. make a frequency distribution, `?table`)
+# 5. Visualise!
+
+# 1. Save the number of rolls
+n_rolls <- 1000
+
