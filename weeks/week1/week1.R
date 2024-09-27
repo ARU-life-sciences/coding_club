5473bbc4dede558d9ac29fc3bbb8a69d31688793
Max Brown <max.carter-brown@aru.ac.uk>
2024-09-27 17:13:24 +0100

    First week upload.

--- weeks/week1/week1.R
+++ weeks/week1/week1.R
@@ -6,10 +6,86 @@
 
 # 1. We need to save the number of rolls.
 # 2. Some way of simulating dice rolls (`?sample`).
-# 3. We have two dice. Each die rolls a number. What are we interested in? The actual values, or the combined value?
+# 3. We have two dice. Each die rolls a number. What are we interested in? 
+# The actual values, or the combined value?
 # 4. Count outcome occurrences (i.e. make a frequency distribution, `?table`)
 # 5. Visualise!
 
 # 1. Save the number of rolls
-n_rolls <- 1000
+n_rolls <- 100000
 
+# the dice numbers
+dice_numbers <- 1:6
+
+# some kind of random number generator
+# use sample
+
+# 2. Simulate dice rolls
+# HELP: can we create a function to generate dice? Yes, we can!
+# make the function
+roll_dice <- function(n_rolls = 1000) {
+  sample(dice_numbers, size = n_rolls, replace = TRUE)
+}
+
+
+dice1 <- roll_dice(n_rolls)
+# this was the first roll of the dice
+dice1[1]
+# and the 100th
+dice1[100]
+# and we want another die
+dice2 <- roll_dice(n_rolls)
+
+# Are there rolls at the same index which were the same?
+dice1 == dice2
+sum(dice1 == dice2)
+# HELP: what's the probability of this happening?
+sum(dice1 == dice2) / n_rolls
+
+# 3. Combine the dice rolls
+# We want to add the two dice rolls together
+counts <- dice1 + dice2
+# show what these counts are:
+counts
+
+# 4. Count the occurrences
+
+# do it manually, and naively
+counts[counts == 5]
+
+for (i in 2:12) {
+  print(paste(i, "occurs", sum(counts == i), "times"))
+}
+
+frequncy_table <- table(counts)
+
+# 5. Visualise!
+barplot(frequncy_table, 
+        main = "Frequency of dice rolls", 
+        xlab = "Sum of dice rolls", 
+        ylab = "Frequency"
+      )
+# HELP: do this in ggplot2
+
+
+
+# And shuffling and drawing from 52 cards:
+
+# 1. We need to make a big data frame of all the cards. 
+#    We don't really want to type this all out though (`?expand.grid`)...
+# 2. And how many cards are we drawing?
+# 3. How are we going to shuffle the deck? (`sample` might be our friend here)
+# 4. Draw the cards. How many times do we want to draw the cards?
+
+suits <- c("hearts", "diamonds", "clubs", "spades")
+values <- c(2:10, "J", "Q", "K", "A")
+
+# 1. Make a big data frame of all the cards
+cards <- expand.grid(suit = suits, value = values)
+
+# make the shuffled indices
+shuffled_indices_of_the_deck <- sample(x = 1:52, size = 52)
+
+# shuffle the deck
+shuffled_deck <- cards[shuffled_indices_of_the_deck, ]
+rownames(shuffled_deck) <- NULL
